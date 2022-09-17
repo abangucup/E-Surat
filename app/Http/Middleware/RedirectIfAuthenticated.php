0e26b70a3666d5a3d6f788b0823029fe2a6184e5
salmanmustapa <46360546+abangucup@users.noreply.github.com>
2022-09-17 15:48:46 +0800

    Revert "Revert "Ucup" (#2)"
    This reverts commit 2808d27e8290183991083cae9728f5e742cada4d.

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\Role;
 use App\Providers\RouteServiceProvider;
 use Closure;
 use Illuminate\Http\Request;
@@ -20,10 +21,16 @@ class RedirectIfAuthenticated
     public function handle(Request $request, Closure $next, ...$guards)
     {
         $guards = empty($guards) ? [null] : $guards;
-
+        $roles = Role::all();
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+                // return redirect(RouteServiceProvider::HOME);
+                foreach ($roles as $role) {
+                    $user = auth()->user()->role_id;
+                    if ($user == $role->id) {
+                        return redirect($role->redirect_to);
+                    }
+                }
             }
         }
 
---
+++ resources/views/layouts/templates/footer.blade.php
@@ -0,0 +1,10 @@
+<footer>
+    <div class="footer clearfix mb-0 text-muted">
+        <div class="float-start">
+            <p>2021 &copy; Sistem Informasi E-Surat</p>
+        </div>
+        <div class="float-end">
+            <p>Created By <i class="bi bi-coffe"></i></span><a href="https://github.com/abangucup" target="_blank">Salman Mustapa</a></p>
+        </div>
+    </div>
+</footer>
