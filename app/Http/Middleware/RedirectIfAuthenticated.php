2808d27e8290183991083cae9728f5e742cada4d
salmanmustapa <46360546+abangucup@users.noreply.github.com>
2022-09-17 15:33:06 +0800

    Revert "Ucup" (#2)

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Middleware;
 
-use App\Models\Role;
 use App\Providers\RouteServiceProvider;
 use Closure;
 use Illuminate\Http\Request;
@@ -21,16 +20,10 @@ class RedirectIfAuthenticated
     public function handle(Request $request, Closure $next, ...$guards)
     {
         $guards = empty($guards) ? [null] : $guards;
-        $roles = Role::all();
+
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                // return redirect(RouteServiceProvider::HOME);
-                foreach ($roles as $role) {
-                    $user = auth()->user()->role_id;
-                    if ($user == $role->id) {
-                        return redirect($role->redirect_to);
-                    }
-                }
+                return redirect(RouteServiceProvider::HOME);
             }
         }
 
--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,18 @@
 <?php
 
-use App\Http\Controllers\AdminController;
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Auth\LogoutController;
-use App\Http\Controllers\DosenController;
-use App\Http\Controllers\MahasiswaController;
-use App\Http\Controllers\SuratController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['guest'])->group(function () {
-    Route::get('/', [LoginController::class, 'index'])->name('login');
-    Route::post('/', [LoginController::class, 'store']);
-});
-Route::group(['middleware' => 'auth'], function () {
-    Route::post('logout', [LogoutController::class, 'store'])->name('logout');
-    // Is Admin
-    Route::group(['middleware' => 'role:admin', 'prefix' => 'admin'], function () {
-        Route::get('/', [AdminController::class, 'index'])->name('admin');
-    });
-    // Is Dosen
-    Route::group(['middleware' => 'role:dosen', 'prefix' => 'dosen'], function () {
-        Route::get('/', [DosenController::class, 'index'])->name('dosen');
-    });
-    // Is Mahasiswa
-    Route::group(['middleware' => 'role:mahasiswa', 'prefix' => 'mahasiswa'], function () {
-        Route::get('/', [MahasiswaController::class, 'index'])->name('mahasiswa');
-    });
-    // Is Surat
-    Route::get('/suratmasuk', [SuratController::class, 'suratmasuk'])->name('suratmasuk');
-    Route::get('/suratkeluar', [SuratController::class, 'suratkeluar'])->name('suratkeluar');
-    Route::get('/surattugas', [SuratController::class, 'surattugas'])->name('surattugas');
-    Route::get('/suratkegiatan', [SuratController::class, 'suratkegiatan'])->name('suratkegiatan');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
 });
